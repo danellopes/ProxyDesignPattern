3bfb5f9cbd5f4fdc0f0348b5738f5ddc94bbee9e
Daniel Lopes <daniel.nunes.meira@outlook.com>
2026-01-20 07:27:35 -0300

    Implemented first Proxy design pattern demo.

---
+++ CSProxy/ProxyDemo.cs
@@ -0,0 +1,57 @@
+﻿namespace DesignPatterns
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Driving the car.");
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+
+        public Driver(int age)
+        {
+            this.Age = age;
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Driver driver;
+        private Car car = new Car();
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if(driver.Age >= 18)
+            {
+                car.Drive();
+            }
+            else
+            {
+                Console.WriteLine("too young");
+            }
+        }
+    }
+
+    public class Program
+    {        
+        static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver(8));
+            car.Drive();
+        }
+    }
+}
